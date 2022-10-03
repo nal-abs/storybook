a83972ae2575e98717fdbee22d4e1a1008e07d43
nvn-laufire <nvn@laufire.in>
2022-10-03 13:05:26 +0530

    used that icon style to check the working.

--- src/stories/AdditionalCharge.js
+++ src/stories/AdditionalCharge.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { faInfo } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const formatPrice = (price) => {
 	// eslint-disable-next-line no-magic-numbers
@@ -9,9 +11,17 @@ const formatPrice = (price) => {
 	return priceInRupee;
 };
 
-const AdditionalCharge = ({ item }) => <div className="price">
-	<span>Hello</span>
-	<div>AED {formatPrice(item.additionalCharge)}</div>
+const PriceNotesIcon = ({ text }) =>
+	text && <span className="price-notes-icon info">
+		<span className="icon" title={ text }>
+			<FontAwesomeIcon icon={ faInfo }/>
+		</span>
+	</span>;
+
+const AdditionalCharge = ({ item }) => <div className="price display-row">
+	<div className="stretch">AED {formatPrice(item.additionalCharge)}</div>
+	<div className="cart-with-items">
+		<PriceNotesIcon text={ item.priceNotes }/></div>
 </div>;
 
 export default AdditionalCharge;
