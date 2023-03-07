1c599b060a562f461ed051f6a07c17b510cb89a4
NPriyadarsini <pdn@abstractit.in>
2023-03-07 20:19:23 +0530

    Created story for MuiSwitch

---
+++ src/stories/Switch.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-console */
+import PropTypes from 'prop-types';
+import { Switch as MuiSwitch } from '@mui/material';
+import * as React from 'react';
+
+const Switch = (context) => {
+	const { ...args } = context;
+
+	return (
+		<MuiSwitch
+			{ ...{ ...args } }
+		/>);
+};
+
+export default Switch;
+
+Switch.propTypes = {
+	context: PropTypes.object,
+};
---
+++ src/stories/Switch.stories.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import Switch from './Switch';
+
+export default {
+	title: 'stories/muiSwitch',
+	component: Switch,
+};
+
+const Template = (args) => <Switch { ...args }/>;
+
+export const muiSwitch = Template.bind({});
+
+muiSwitch.args = {
+	size: 'large',
+	color: 'success',
+	checked: false,
+	disabled: false,
+};
