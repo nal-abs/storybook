3fa58e65ba836c2512f68188f202fe866305f23c
nvn-laufire <nvn@laufire.in>
2023-03-09 12:14:40 +0530

    ActionSet stories introduced.

---
+++ src/stories/ActionSet.stories.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import ActionSet from './ActionSet';
+
+export default {
+	title: 'stories/ActionSet',
+	component: ActionSet,
+	argTypes: {
+		direction: {
+			type: 'select',
+			options: ['up', 'down', 'left', 'right'],
+		},
+		tooltipPlacement: { type: 'select',
+			options: ['bottom-end',
+				'bottom-start',
+				'bottom',
+				'left-end',
+				'left-start',
+				'left',
+				'right-end',
+				'right-start',
+				'right',
+				'top-end',
+				'top-start',
+				'top']		},
+		hidden: {
+			type: Boolean,
+			options: [true, false],
+		},
+		tooltipOpen: { type: Boolean, options: [true, false] },
+	},
+};
+
+const Template = (args) => <ActionSet { ...args }/>;
+
+export const ActionSetStories = Template.bind({});
+
+ActionSetStories.args = {
+	type: 'menu',
+	trigger: {
+		children: {
+			text: 'Speed Dial Icon',
+		},
+	},
+	data: [
+		{ icon: 'Save', children: 'Save' },
+		{ icon: 'Print', children: 'Print' },
+		{ icon: 'Share', children: 'Share' },
+	],
+	sx: {
+		'width': '200px',
+		'&:active': {
+			color: 'primary.main',
+		},
+		'&:hover': {
+			backgroundColor: '#e3f2fd',
+		},
+	},
+
+};
