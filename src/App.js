d72a6e5307c28480475d01554af6d5c5b2169393
nvn-laufire <nvn@laufire.in>
2023-03-14 12:56:21 +0530

    Introduced Data grid in basic stage.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { React } from 'react';
 import './App.scss';
-import TabWrapper from './components/TabsWrapper';
+import DataGrid from './components/DataGrid';
 
 const App = () =>
 
 	<div className="App" role="App">
-		<TabWrapper/>
+		<DataGrid/>
 	</div>;
 
 export default App;
