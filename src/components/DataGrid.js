33732d03dc5de0453455410cff551895ba5926bc
nvn-laufire <nvn@laufire.in>
2023-03-15 13:14:40 +0530

    RowHeight props and button name varies based on given value.

--- src/components/DataGrid.js
+++ src/components/DataGrid.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { DataGrid as MuxDataGrid } from '@mui/x-data-grid';
 import MuiSelect from '../stories/Select';
+import { Button } from '@mui/material';
 
 const args = {
 	options: ['Ten', 'Twenty', 'Thirty'],
@@ -19,9 +20,10 @@ const args = {
 const DropDown = <MuiSelect { ...args }/>;
 
 const rows = [
-	{ id: 1, col1: 'Hello', col2: 'World', age: 25 },
-	{ id: 2, col1: 'DataGridPro', col2: 'is Awesome', age: 27 },
-	{ id: 3, col1: 'MUI', col2: 'is Amazing', age: 30 },
+	{ id: 1, col1: 'Hello', col2: 'World', age: 25, Button: 'Edit' },
+	{ id: 2, col1: 'DataGridPro', col2: 'is Awesome', age: 27,
+		Button: 'Update' },
+	{ id: 3, col1: 'MUI', col2: 'is Amazing', age: 30, Button: 'Delete' },
 ];
 
 const columns = [
@@ -29,13 +31,16 @@ const columns = [
 	{ field: 'Select', headerName: 'Select', width: 250,
 		renderCell: () => DropDown },
 	{ field: 'col2', headerName: 'Column 2', width: 150 },
+	{ field: 'Button', headerName: 'Button', width: 150,
+		renderCell: (cellValues) => <Button variant="contained">
+			{cellValues.value}</Button> },
 	{ field: 'age', headerName: 'Age', width: 100, type: 'number',
 		editable: true },
 ];
 
 const DataGrid = () =>
 	<div style={ { height: 300, width: '100%' } }>
-		<MuxDataGrid rows={ rows } columns={ columns }/>
+		<MuxDataGrid rows={ rows } columns={ columns } rowHeight={ 100 }/>
 	</div>
 	;
 
