6209c75c326c71b83c56636348d7868f37444c1b
NPriyadarsini <pdn@abstractit.in>
2023-03-17 18:01:42 +0530

    created and improved splash screen

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
-import { React } from 'react';
+/* eslint-disable no-mixed-spaces-and-tabs */
+import { React, useEffect, useState } from 'react';
 import './App.scss';
 import DataGrid from './components/Datagrid';
+import image from '../src/assets/splash_icon/splash_icon.png';
 
-const App = (context) => <div className="App" role="App">
-	<DataGrid { ...{ ...context,
-		data: {
-			collection: 'journals',
-		}} }
-	/>
-</div>;
+const milliseconds = 8000;
+// eslint-disable-next-line max-lines-per-function
+const App = (context) => {
+	const [loading, setLoading] = useState(false);
+
+	useEffect(() => {
+		setLoading(true);
+		setTimeout(() => {
+			setLoading(false);
+		}, milliseconds);
+	}, []);
+	return (
+		<div className="App" role="App">
+			{
+				loading
+					? <div className="splash">
+						<img
+							className="image fade-in"
+							src={ image }
+							alt="image"
+						/>
+					</div>
+					:				<DataGrid		{ ...{ ...context,
+						data:
+						{
+							collection: 'journals',
+						}}
+					}
+					     />
+			},
+		</div>
+	);
+};
 
 export default App;
