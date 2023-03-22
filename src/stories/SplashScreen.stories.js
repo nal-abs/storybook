7c1dc524f35b0e413d3ff22f5f07554d23efdee9
NPriyadarsini <pdn@abstractit.in>
2023-03-22 20:14:58 +0530

    Improved SplashScreen Stories in storybook

--- src/stories/SplashScreen.stories.js
+++ src/stories/SplashScreen.stories.js
@@ -1,16 +1,38 @@
 import * as React from 'react';
 import SplashScreen from './SplashScreen';
-
+import SplashImage from '../stories/examples/SplashImage';
+import SplashText from '../stories/examples/SplashText';
 export default {
 	title: 'stories/SplashScreen',
 	component: SplashScreen,
+	argTypes: {
+		child: {
+			control: {
+				type: 'select',
+				options: ['SplashImage', 'SplashText'],
+			},
+		},
+	},
 };
 
-const Template = (args) => <SplashScreen { ...args }/>;
+const childComponents = {
+	SplashImage,
+	SplashText,
+};
+
+const Template = ({ child, text, ...rest }) => {
+	const SelectedChild = childComponents[child];
+
+	return <SplashScreen { ...rest }>
+		<SelectedChild>{text}</SelectedChild>
+	</SplashScreen>;
+};
 
 export const muiSplash = Template.bind({});
 
 muiSplash.args = {
 	textColor: 'white',
 	backgroundColor: 'black',
+	child: 'SplashImage',
+	text: '',
 };
