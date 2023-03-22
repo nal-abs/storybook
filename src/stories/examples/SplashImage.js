2e6e5f4c7fd110491b4e2f88968c7c3bca9e9864
NPriyadarsini <pdn@abstractit.in>
2023-03-22 16:25:46 +0530

    added splashImage , splashText as examples in storybook

---
+++ src/stories/examples/SplashImage.js
@@ -0,0 +1,7 @@
+import { React } from 'react';
+import image from '../../assets/splash_icon/Splash_rotate.gif';
+
+const SplashImage = () =>
+	<img src={ image }/>;
+
+export default SplashImage;
---
+++ src/stories/examples/SplashText.js
@@ -0,0 +1,6 @@
+import { React } from 'react';
+
+const SplashText = ({ children }) =>
+	<h1>{children}</h1>;
+
+export default SplashText;
