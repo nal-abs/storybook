25815a9eac3208421472aeaeaece03098c506e13
NPriyadarsini <pdn@abstractit.in>
2023-04-04 20:42:40 +0530

    Added controls,event handlers in videoPlayer Component

--- src/stories/VideoPlayer.stories.js
+++ src/stories/VideoPlayer.stories.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import * as React from 'react';
 import VideoPlayer from '../stories/common/VideoPlayer';
 
@@ -11,24 +12,30 @@ const Template = (args) => <VideoPlayer { ...args }/>;
 export const videoPlayer = Template.bind({});
 
 videoPlayer.args = {
-	url: '',
+	url: 'https://youtu.be/D-rHu8vxrHI',
+	pip: false,
 	playing: false,
 	loop: false,
-	controls: false,
+	controls: true,
 	light: false,
 	volume: 0,
 	muted: false,
-	playbackRate: 1,
-	width: '',
-	height: '',
-	style: {},
-	progressInterval: 1000,
-	playsinline: false,
-	pip: false,
-	stopOnUnmount: true,
-	fallback: null,
-	// wrapper: false,
-	// playIcon: false,
-	previewTabIndex: 0,
-	// config: false,
+	playbackRate: 1.0,
+	value: {
+		url: 'https://youtu.be/D-rHu8vxrHI',
+		pip: false,
+		ready: '',
+		start: '',
+		playing: true,
+		controls: false,
+		light: false,
+		volume: 0.8,
+		muted: false,
+		played: 0,
+		loaded: 0,
+		duration: 0,
+		playbackRate: 1.0,
+		loop: false,
+	},
+	onChange: ({ target }) => console.log(target.value),
 };
--- src/stories/common/VideoPlayer.js
+++ src/stories/common/VideoPlayer.js
@@ -1,10 +1,83 @@
-import React from 'react';
+/* eslint-disable no-console */
+/* eslint-disable max-lines-per-function */
+import { nothing } from '@laufire/utils/fn';
+import React, { useState } from 'react';
 import ReactPlayer from 'react-player/lazy';
 
+const transformEvent = (value) => ({
+	target: {
+		value,
+	},
+});
 // Lazy load the YouTube player
-const VideoPlayer = (props) =>
-	<ReactPlayer
-		{ ...props }
-	/>;
+const VideoPlayer = ({ url, pip, playing, controls,
+	light, loop, playbackRate, volume, muted,
+	onChange = nothing, value: initialValue, ...args }) => {
+	const [value, setValue] = useState(initialValue);
+
+	return (
+		<ReactPlayer
+			{ ...args }
+			url={ url }
+			pip={ pip }
+			playing={ playing }
+			controls={ controls }
+			light={ light }
+			loop={ loop }
+			playbackRate={ playbackRate }
+			volume={ volume }
+			muted={ muted }
+			onReady={ () => {
+				setValue({ ready: 'ready' });
+				onChange(transformEvent({ ...value, ready: 'ready' }));
+			} }
+			onStart={ () => {
+				setValue({ start: 'start' });
+				onChange(transformEvent({ ...value, start: 'start' }));
+			} }
+			onPlay={ () => {
+				setValue({ playing: true });
+				onChange(transformEvent({ ...value,
+					playing: !value.playing }));
+			} }
+			onProgress={ ({ target }) => {
+				setValue({ played: parseFloat(target.value) });
+				onChange(transformEvent({
+					played: parseFloat(target.value),
+				}));
+			} }
+			onSeek={ ({ target }) => {
+				setValue({ played: parseFloat(target.value) });
+				onChange(transformEvent({ ...value,
+					played: parseFloat(target.value) }));
+			} }
+			onPause={ () => {
+				setValue({ playing: false });
+				onChange(transformEvent({ ...value,
+					playing: !value.playing }));
+			} }
+			onDuration={ (duration) => {
+				setValue({ duration });
+				onChange(transformEvent({ ...value, duration }));
+			} }
+			onPlaybackRateChange={ (speed) => {
+				setValue({ playbackRate: parseFloat(speed) });
+				onChange(transformEvent({ ...value,
+					playbackRate: parseFloat(speed) }));
+			} }
+			onEnded={ () => {
+				setValue({ playing: value.loop });
+			} }
+			onEnablePIP={ () => {
+				setValue({ pip: true });
+				onChange(transformEvent({ ...value, pip: true }));
+			} }
+			onDisablePIP={ () => {
+				setValue({ pip: false });
+				onChange(transformEvent({ ...value, pip: false }));
+			} }
+		/>
+	);
+};
 
 export default VideoPlayer;
