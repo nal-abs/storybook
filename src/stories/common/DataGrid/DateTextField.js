5e7c69ddd9d1a51655c27d0971515434801d44e1
nvn-laufire <nvn@laufire.in>
2023-04-12 12:25:18 +0530

    Error message popup when out of limit date entered in date field.

--- src/stories/common/DataGrid/DateTextField.js
+++ src/stories/common/DataGrid/DateTextField.js
@@ -6,12 +6,30 @@ import { peek } from '@laufire/utils/debug';
 import { TextField } from '@mui/material';
 import dayjs from 'dayjs';
 import React, { useState } from 'react';
+import Ajv from 'ajv';
+import addFormats from 'ajv-formats';
+
+const isDateValid = (value, schema) => {
+	const newValue = dayjs(value).format('YYYY-MM-DD');
+	const ajv = new Ajv();
+
+	const validate = addFormats(ajv).compile(schema);
+
+	const valid = validate(newValue);
+
+	return valid;
+};
 
 const DateTextField = (context) => {
 	const { params: { value, row, field }, data,
 		data: { formatMaximum, formatMinimum }} = context;
 
 	const [date, setDate] = useState(dayjs(value).format('YYYY-MM-DD'));
+
+	const error = isDateValid(date, data)
+		? { helperText: ' ' }
+		: { error: true,
+			helperText: 'Incorrect entry!' };
 
 	return (
 		<TextField { ...{
@@ -27,9 +45,7 @@ const DateTextField = (context) => {
 			variant: 'standard',
 			value: date,
 			InputProps: { disableUnderline: true },
-			onKeyPress: (e) => {
-				e.preventDefault();
-			},
+			...error,
 		} }
 		/>);
 };
