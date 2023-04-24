9611f63dc7535ce391f74981ec2aa4053dbd510d
nvn-laufire <nvn@laufire.in>
2023-04-24 13:31:59 +0530

    Refactored dataGrid and Removed unused style sheet.

--- src/stories/Datagrid.stories.js
+++ src/stories/Datagrid.stories.js
@@ -46,6 +46,8 @@ DataGrid.args = {
 					type: 'string',
 					format: 'date',
 					title: 'Date',
+					formatMinimum: '2013-11-17',
+					formatMaximum: '2023-06-06',
 				},
 				credit: {
 					type: 'string',
