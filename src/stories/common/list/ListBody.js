695efdb45e11dc05c2055bf9cf043288ef080136
njj-abs <njj@abstractit.in>
2023-05-04 10:43:57 +0530

    Drag and Drop for list.

--- src/stories/common/list/ListBody.js
+++ src/stories/common/list/ListBody.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { useDrag, useDrop } from 'react-dnd';
+import ReactTableReorder from '../../../helper/ReactTableReorder';
+import { Box } from '@mui/material';
+
+const Body = (context) => {
+	const dropRef = useRef();
+	const position = 'row';
+
+	const [, drop] = useDrop(ReactTableReorder
+		.getDrop({ ...context, ref: dropRef, position: position }));
+
+	const [{ isDragging }, drag] = useDrag(ReactTableReorder
+		.getDrag({ ...context, position }));
+
+	const opacity = isDragging ? 0 : 1;
+
+	drag(drop(dropRef));
+
+	return (
+		<Box { ...{ ref: dropRef, style: { opacity }} }>
+			<context.Component { ...context }/>
+		</Box>
+	);
+};
 
 const ListBody = (context) => {
-	const { value, Component, props: { prepareRow }} = context;
+	const { value, props: { prepareRow }} = context;
 
 	return value.map((data, key) => prepareRow(data)
-			|| <Component key={ key } { ...{ ...context, data } }/>);
+			|| <Body key={ key } { ...{ ...context, data } }/>);
 };
 
 export default ListBody;
