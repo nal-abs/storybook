07afe642e058f73d330782fc85ec27111d82deff
NPriyadarsini <pdn@abstractit.in>
2023-05-08 19:36:00 +0530

    Added arguments in imageCarousel

--- src/stories/ImageCarousel.stories.js
+++ src/stories/ImageCarousel.stories.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-console */
+/* eslint-disable no-inline-comments */
 import ImageCarousel from './common/ImageCarousel';
 import Debugger from './common/Debugger';
 
@@ -14,6 +16,45 @@ export const imageCarousel = Template.bind({});
 
 imageCarousel.args = {
 	count: 4,
-	spaceBetween: 50,
+	direction: 'horizontal', /* 'horizontal' | 'vertical' */
+	loop: 'false',
+	modules: [],
+	watchOverflow: true,
+	onAutoPlay: {
+		speed: 300,
+		followFinger: 'true',
+		grabCursor: 'true',
+		effect: 'slide', /* 'slide' | 'fade' | 'cube' | 'coverflow' | 'flip' | 'creative' | 'cards'*/
+	},
+	spaceBetween: 10,
 	slidesPerView: 1,
+	breakpoints: {
+		// when window width is >= 320px
+		320: {
+			slidesPerView: 2,
+			spaceBetween: 20,
+		},
+		// when window width is >= 480px
+		480: {
+			slidesPerView: 3,
+			spaceBetween: 30,
+		},
+		// when window width is >= 640px
+		640: {
+			slidesPerView: 4,
+			spaceBetween: 40,
+		},
+		720: {
+			slidesPerView: 5,
+			spaceBetween: 50,
+		},
+		1080: {
+			slidesPerView: 6,
+			spaceBetween: 60,
+		},
+	},
+	onAny: (eventName, ...args) => {
+		console.log('Event: ', eventName);
+		console.log('Event data: ', args);
+	},
 };
