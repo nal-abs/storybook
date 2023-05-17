8b2c4f5062894350c225bd7ec1716309c1df0cef
nal-abs <nal-abs@abstractit.in>
2023-05-17 16:57:39 +0530

    Removed unused Date folders.

--- src/stories/common/DataGrid/DateTime/Input.js
+++
@@ -1,9 +0,0 @@
-import validate from './validate';
-import genInputField from '../genInputField';
-
-const Input = genInputField({
-	validate,
-
-});
-
-export default Input;
--- src/stories/common/DataGrid/Time/index.js
+++
@@ -1,9 +0,0 @@
-import Component from './Input';
-import genInput from '../genInput';
-
-const Time = genInput({
-	type: 'time',
-	Component: Component,
-});
-
-export default Time;
