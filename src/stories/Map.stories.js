1f75079e6b0b91e36e86e5c48f86a8f98fa5f067
NPriyadarsini <pdn@abstractit.in>
2023-05-19 20:29:11 +0530

    Created Story for Map Component

---
+++ src/stories/Map.stories.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-magic-numbers */
+import { React } from 'react';
+import Maps from './common/Map/index';
+
+const component = {
+	title: 'stories/maps',
+	component: Maps,
+};
+
+export default component;
+
+const Template = () => <Maps/>;
+
+export const maps = Template.bind({});
+
+maps.args = {
+
+};
