e1b75ce750526b592f269aefeb080ba77252e18d
njj-abs <njj@abstractit.in>
2023-05-20 13:36:06 +0530

    Fixed the marker issue.

--- src/stories/common/Map/Location.js
+++ src/stories/common/Map/Location.js
@@ -2,10 +2,19 @@
 /* eslint-disable no-magic-numbers */
 import { React, useEffect, useState } from 'react';
 import { useMap, Marker, Popup, Circle } from 'react-leaflet';
+import Leaflet from 'leaflet';
 
 const Location = () => {
 	const map = useMap();
 	const [coordinates, setCoordinates] = useState(null);
+	const icon = new Leaflet.Icon({
+		iconUrl: '/images/marker-icon.png',
+		shadowUrl: '/images/marker-shadow.png',
+		iconSize: [25, 41],
+		iconAnchor: [12, 41],
+		popupAnchor: [1, -34],
+		shadowSize: [41, 41],
+	});
 
 	useEffect(() => {
 		map.locate({
@@ -26,7 +35,7 @@ const Location = () => {
 				sfillOpacity={ 0.1 }
 				radius={ 500 }
 			/>
-			<Marker position={ coordinates }>
+			<Marker position={ coordinates } { ...{ icon } }>
 				<Popup>You are here</Popup>
 			</Marker>
 		</>
