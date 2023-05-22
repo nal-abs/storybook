3fd14ecf753ee19c8f7d182d1b237745f160dc81
njj-abs <njj@abstractit.in>
2023-05-22 18:20:58 +0530

    updated the storybook and installed the leaflet.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,14 @@
-module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-		'@storybook/preset-scss',
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-  ],
-  "framework": "@storybook/react"
+/** @type { import('@storybook/react-webpack5').StorybookConfig } */
+const config = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/preset-create-react-app', '@storybook/addon-interactions'],
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+  staticDirs: ['../public'],
 }
+export default config
