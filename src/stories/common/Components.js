ddd164cd7ee24771e7fa4c9121b0d01b3f9abd39
nal-abs <nal-abs@abstractit.in>
2023-05-22 14:01:51 +0530

    Bug fixed.

--- src/stories/common/Components.js
+++ src/stories/common/Components.js
@@ -1,23 +1,9 @@
-import Button from './Button';
-import CheckBox from './CheckBox';
-import Typography from './Typography';
-import Drawer from './Drawer/index';
-import Menu from './Menu/index';
-import DataGrid from './DataGrid/index';
 import Journal from '../../components/Journal';
 import Ledger from '../../components/Ledger';
-import Tab from './Tab/index';
 
 const Components = {
-	Button,
-	CheckBox,
-	Menu,
-	Typography,
-	Tab,
-	Drawer,
 	Journal,
 	Ledger,
-	DataGrid,
 };
 
 export default Components;
