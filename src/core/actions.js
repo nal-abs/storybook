8bc3767f636554a5f877bf0be964099a0e8ff83e
nal-abs <nal-abs@abstractit.in>
2023-05-27 12:32:48 +0530

    Eslint error corrected in action file.

--- src/core/actions.js
+++ src/core/actions.js
@@ -1,25 +1,16 @@
 import DirectionManager from '../services/DirectionManager';
 
 const actions = {
-	selectedTab: (context) => ({
-		value: context.data,
-	}),
+	selectedTab: (context) => ({ value: context.data }),
 	setDirection: (context) => ({
 		open: DirectionManager.setDirection(context),
 		anchor: context.data,
 	}),
-	closeDrawer: (context) => ({
-		open: DirectionManager.closeDrawer(context),
-	}),
-	setLoading: () => ({
-		loading: true,
-	}),
-	endLoading: () => ({
-		loading: false,
-	}),
-	deleteRow: ({ data: { rows, value, name }}) => ({
-		[name]: rows.filter((row) => row.id !== value.id),
-	}),
+	closeDrawer: (context) => ({ open: DirectionManager.closeDrawer(context) }),
+	setLoading: () => ({ loading: true }),
+	endLoading: () => ({ loading: false }),
+	deleteRow: ({ data: { rows, value, name }}) =>
+		({ [name]: rows.filter((row) => row.id !== value.id) }),
 	updateRow: ({ data: { rows, value, name }}) => ({
 		[name]: rows.map((row) => (row.id !== value.id
 			? row
