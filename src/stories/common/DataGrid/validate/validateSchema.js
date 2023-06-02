c0cc34a3f2b0fdac09d78b563e3b03bdbcd13106
nal-abs <nal-abs@abstractit.in>
2023-06-02 16:28:13 +0530

    Refactored.

--- src/stories/common/DataGrid/validate/validateSchema.js
+++ src/stories/common/DataGrid/validate/validateSchema.js
@@ -1,41 +1,9 @@
 import addFormats from 'ajv-formats';
 import Ajv from 'ajv';
+import validateMultipleOf from './validateMultipleOf';
 
 const ajv = new Ajv();
 const phoneNoPattern = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/;
-
-const ten = 10;
-const decimalLength = 2;
-
-const decimal = (number) => {
-	const decimalParts = number.toString().split('.');
-
-	return decimalParts.length === decimalLength
-		? decimalParts[1].length
-		: 0;
-};
-
-const getFractionPoint = (number) =>
-	(isNaN(number) ? 0 : decimal(number));
-
-const hasReminder = (schemaNum, testNum) => {
-	const testNumDecimals = getFractionPoint(testNum);
-	const schemaNumDecimals = getFractionPoint(schemaNum);
-
-	const maxDecimalNum = Math.max(testNumDecimals, schemaNumDecimals);
-	const multiplier = Math.pow(ten, maxDecimalNum);
-
-	return Math.round(testNum * multiplier)
-% Math.round(schemaNum * multiplier) !== 0;
-};
-
-const isTypeNumber = (schemaNum, testNum) => schemaNum === 0
-|| !(typeof testNum === 'number' && isFinite(testNum));
-
-const validateMultipleOf = (schemaNum, testNum) =>
-	(isTypeNumber(schemaNum, testNum)
-		? false
-		: !hasReminder(schemaNum, testNum));
 
 ajv.removeKeyword('multipleOf');
 
