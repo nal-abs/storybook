27c0b7e54582badc63887ce36c5dabf12be64530
nal-abs <nal-abs@abstractit.in>
2023-06-09 12:51:19 +0530

    Introduced onchange event trigged when row is updated.

--- src/stories/common/DataGrid/helper/getColumns.js
+++ src/stories/common/DataGrid/helper/getColumns.js
@@ -1,8 +1,10 @@
+/* eslint-disable max-lines-per-function */
 import React from 'react';
 import { map, values } from '@laufire/utils/collection';
 import inputType from './inputType';
 import { nothing } from '@laufire/utils/fn';
 import SchemaInput from '../SchemaInput';
+import buildEvent from '../../helper/buildEvent';
 
 const formatMap = {
 	'date-time': 'dateTime',
@@ -11,10 +13,20 @@ const formatMap = {
 	'phoneNo': 'phoneNo',
 };
 
+const getRenderCellProp = (
+	ele, key, { editable, width }
+) => ({
+	...ele,
+	headerName: ele.title,
+	field: key,
+	editable: editable,
+	width: width,
+});
+
 const widgetMap = { password: 'password' };
 
 const getColumns = (props) => {
-	const { columns: { data, editable, width }} = props;
+	const { columns: { data }, columns, onChange = nothing } = props;
 
 	return values(map(data.properties, (ele, key) => {
 		const { format, widget, type } = ele;
@@ -22,14 +34,19 @@ const getColumns = (props) => {
 		const getColumnProps = inputType[component] || nothing;
 
 		return {
-			...ele,
-			headerName: ele.title,
-			field: key,
-			editable: editable,
-			width: width,
+			...getRenderCellProp(
+				ele, key, columns
+			),
 			renderCell: (params) =>
-				<SchemaInput
-					{ ...{ value: params.value, schema: ele } }
+				<SchemaInput { ...{
+					value: params.value, schema: ele,
+					onChange: (evt) => {
+						onChange(buildEvent({
+							...params.row,
+							[params.field]: evt.target.value,
+						}));
+					},
+				} }
 				/>,
 			...getColumnProps(),
 		};
